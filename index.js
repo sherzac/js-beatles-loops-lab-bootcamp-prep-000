1685c57aa852df4eceff52d482b9038a44ab1205
Shaun Rezac <shaun.rezac@gmail.com>
2018-05-07 15:29:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (let i = 0; i < musicians.length; i++) {
@@ -25,5 +26,11 @@ function iLoveTheBeatles(i) {
     array.push("I love the Beatles!");
     i++;
   } while (i < 15);
+=======
+function theBeatlesPlay(array) {
+  for (let i = 0; i < 4; i++) {
+    array.push(musicians.length(i))
+  }
+>>>>>>> dfc18b35d218609d97df9bc85d2ca538602efbea
   return array
 }
